49080f8d7d1923e91c5d457ca3cd6033372cc4af
reemo <r48n34@gmail.com>
2023-04-20 11:30:12 +0800

    Remove unused file

--- test/yoloRoute/YoloService.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from 'knex';
-export class YoloService {
-    constructor(private knex: Knex) {}
-    async getUser(): Promise<object[]> {
-        return await this.knex.select('*').from('users');
-    }
-}
